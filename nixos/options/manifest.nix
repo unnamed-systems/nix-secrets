ed8220ca43c06ee288f686db5e14660f70cccb6a
yunfachi <git@ynf.sh>
2026-07-22 16:52:42 +0000

    nixos/options/manifest: return lists instead of attrsets

--- nixos/options/manifest.nix
+++ nixos/options/manifest.nix
@@ -11,7 +11,7 @@ in
     description = "TODO";
     type = lib.types.str;
     default = builtins.toJSON {
-      inherit (cfg) secrets;
+      secrets = builtins.attrValues cfg.secrets;
     };
     readOnly = true;
     internal = true;
