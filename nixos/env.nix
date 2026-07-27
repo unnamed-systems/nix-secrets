afe07f647406fc886105d9f63ac9610e14068fa6
yunfachi <git@ynf.sh>
2026-07-27 10:52:57 +0000

    nixos/env: add storagePath option

--- nixos/env.nix
+++ nixos/env.nix
@@ -12,14 +12,30 @@ in
       # example = TODO;
     };
 
-    installPackage = lib.mkEnableOption "installation of `security.nix-secrets.package` into `environment.systemPackages`" // {
-      default = true;
+    storagePath = lib.mkOption {
+      description = "TODO";
+      type = lib.types.nullOr (lib.types.pathWith {
+        inStore = false;
+        absolute = true;
+      });
+      default = null;
+      example = "/etc/nixos/secrets";
     };
+
+    installPackage =
+      lib.mkEnableOption "installation of `security.nix-secrets.package` into `environment.systemPackages`"
+      // {
+        default = true;
+      };
   };
 
   config = lib.mkIf cfg.enable {
     environment = {
-      variables.NIX_SECRETS_NIX_EVAL_COMMAND = cfg.nixEvalCommand;
+      variables = {
+        NIX_SECRETS_NIX_EVAL_COMMAND = cfg.nixEvalCommand;
+        NIX_SECRETS_STORAGE_PATH = cfg.storagePath;
+      };
+
       systemPackages = lib.optional cfg.installPackage cfg.package;
     };
   };
