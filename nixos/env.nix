8c1c2b2e61786c17f58966dd9daf7c6c8163bf7d
yunfachi <git@ynf.sh>
2026-07-27 10:06:28 +0000

    nixos/env: add installPackage

--- nixos/env.nix
+++ nixos/env.nix
@@ -11,9 +11,16 @@ in
       defaultText = lib.literalExpression "\${lib.getExe config.nix.package} --extra-experimental-features 'nix-command flakes' eval --raw";
       # example = TODO;
     };
+
+    installPackage = lib.mkEnableOption "installation of `security.nix-secrets.package` into `environment.systemPackages`" // {
+      default = true;
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    environment.variables.NIX_SECRETS_NIX_EVAL_COMMAND = cfg.nixEvalCommand;
+    environment = {
+      variables.NIX_SECRETS_NIX_EVAL_COMMAND = cfg.nixEvalCommand;
+      systemPackages = lib.optional cfg.installPackage cfg.package;
+    };
   };
 }
