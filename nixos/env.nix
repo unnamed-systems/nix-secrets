ab792ef9434c08f2cb9dcf734967507d79e123b9
yunfachi <git@ynf.sh>
2026-07-27 09:33:14 +0000

    nixos/env: allow null nixEvalCommand
    Null values are ignored by environment.variables.

--- nixos/env.nix
+++ nixos/env.nix
@@ -6,7 +6,7 @@ in
   options.security.nix-secrets = {
     nixEvalCommand = lib.mkOption {
       description = "TODO";
-      type = lib.types.str;
+      type = lib.types.nullOr lib.types.str;
       default = "${lib.getExe config.nix.package} --extra-experimental-features 'nix-command flakes' eval --raw";
       defaultText = lib.literalExpression "\${lib.getExe config.nix.package} --extra-experimental-features 'nix-command flakes' eval --raw";
       # example = TODO;
