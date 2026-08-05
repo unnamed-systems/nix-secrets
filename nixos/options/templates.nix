a4bdee3ea555445125d40cbdd353ce3bfe425252
yunfachi <git@ynf.sh>
2026-08-05 20:53:44 +0300

    nixos/options/templates: add descriptions

--- nixos/options/templates.nix
+++ nixos/options/templates.nix
@@ -8,14 +8,25 @@ let
       options = {
 
         name = lib.mkOption {
-          description = "TODO";
+          description = ''
+            Template name used as part of the default path.
+          '';
           type = lib.types.str;
           default = name;
           # example = TODO;
         };
 
         content = lib.mkOption {
-          description = "TODO";
+          description = ''
+            Template content. May be either a string or a path to file.
+
+            Secret references can be inserted using
+            `security.nix-secrets.secrets.<name>.templateKey` or by converting a secret
+            option to a string: `"''${security.nix-secrets.secrets.<name>}"`.
+
+            References are replaced with the corresponding secret values when the template
+            is activated.
+          '';
           type =
             let
               inputType = lib.types.either lib.types.str outputType; # TODO: inputType = lib.types.str;
@@ -32,7 +43,11 @@ let
         };
 
         neededForUsers = lib.mkOption {
-          description = "TODO";
+          description = ''
+            Whether the template must be available before users and groups are created.
+
+            Enable this for templates referenced before or during user creation.
+          '';
           type = lib.types.bool;
           default = false;
           example = true;
@@ -40,7 +55,12 @@ let
 
         # Mounting
         path = lib.mkOption {
-          description = "TODO";
+          description = ''
+            Path where the rendered template file will be mounted.
+
+            By default, the template is mounted under "/run/nix-secrets/templates", or under
+            "/run/nix-secrets-for-users/templates" when `neededForUsers` is enabled.
+          '';
           type = lib.types.str;
           # Use separate directories because activation scripts with
           # `beforeUsers = true` and `beforeUsers = false` run independently.
@@ -49,14 +69,24 @@ let
         };
 
         mode = lib.mkOption {
-          description = "TODO";
+          description = ''
+            Permissions of the file created at `path`.
+          '';
           type = lib.types.str;
           default = "0400";
           # example = TODO;
         };
 
         owner = lib.mkOption {
-          description = "TODO";
+          description = ''
+            Owner of the file created at `path`.
+
+            May be specified as either a user name or a numeric UID.
+
+            Cannot be set when `neededForUsers` is enabled because users and groups are
+            not available at this stage of activation. In this case, the file owner is
+            set to `root`.
+          '';
           type = lib.types.oneOf [
             lib.types.int
             lib.types.str
@@ -66,7 +96,15 @@ let
         };
 
         group = lib.mkOption {
-          description = "TODO";
+          description = ''
+            Group of the file created at `path`.
+
+            May be specified as either a group name or a numeric GID.
+
+            Cannot be set when `neededForUsers` is enabled because users and groups are
+            not available at this stage of activation. In this case, the file group is
+            set to `root`.
+          '';
           type = lib.types.oneOf [
             lib.types.int
             lib.types.str
@@ -81,7 +119,9 @@ let
 in
 {
   options.security.nix-secrets.templates = lib.mkOption {
-    description = "TODO";
+    description = ''
+      Templates managed by Nix-Secrets.
+    '';
     type = lib.types.attrsOf templateSubmodule;
     default = { };
     # example = TODO;
