cab1800918f5d0ac45eb1474e1edf92926f633aa
bananad3v <bananad3v@proton.me>
2026-08-05 10:20:47 +0000

    src/main: set default log level to trace in debug

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate tracing;
 
+use tracing::level_filters::LevelFilter;
+
 use crate::command::Args;
 use std::str::FromStr as _;
 
@@ -19,11 +21,20 @@ pub static SECRETS_FOR_USERS_DIR: &str = "/run/nix-secrets-for-users";
 pub static SECRETS_EXTENSION: &str = "enc";
 
 fn main() -> Result<()> {
-    let filter = tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-        #[expect(clippy::expect_used, reason = "Environment filter is hardcoded")]
-        tracing_subscriber::EnvFilter::from_str("error,nix-secrets=info")
-            .expect("Environment filter must work")
-    });
+    let default_level = if cfg!(debug_assertions) {
+        LevelFilter::TRACE
+    } else {
+        LevelFilter::INFO
+    };
+
+    let filter = tracing_subscriber::EnvFilter::builder()
+        .with_default_directive(default_level.into())
+        .from_env()
+        .unwrap_or_else(|_| {
+            #[expect(clippy::expect_used, reason = "Environment filter is hardcoded")]
+            tracing_subscriber::EnvFilter::from_str("error,nix-secrets=info")
+                .expect("Environment filter must work")
+        });
     tracing_subscriber::fmt()
         .compact()
         .with_env_filter(filter)
