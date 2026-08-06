11ca91ca13a043e1ecf16c3234584e0ee0ba7d5a
yunfachi <git@ynf.sh>
2026-08-06 15:40:23 +0300

    tests/shared/flake: format

--- tests/shared/flake.nix
+++ tests/shared/flake.nix
@@ -3,20 +3,22 @@
 
   inputs.nix-secrets.url = "path:../../.";
 
-  outputs = { self, nix-secrets, ... }: {
-    nixosConfigurations = {
-      "shared" = nix-secrets.inputs.nixpkgs.lib.nixosSystem {
-        modules = [
-          nix-secrets.nixosModules.default
-          ./default.nix
-          {
-            nixpkgs.hostPlatform = "x86_64-linux";
-            fileSystems."/".device = "/dev/vda";
-            fileSystems."/".fsType = "ext4";
-            boot.loader.grub.device = "nodev";
-          }
-        ];
+  outputs =
+    { self, nix-secrets, ... }:
+    {
+      nixosConfigurations = {
+        "shared" = nix-secrets.inputs.nixpkgs.lib.nixosSystem {
+          modules = [
+            nix-secrets.nixosModules.default
+            ./default.nix
+            {
+              nixpkgs.hostPlatform = "x86_64-linux";
+              fileSystems."/".device = "/dev/vda";
+              fileSystems."/".fsType = "ext4";
+              boot.loader.grub.device = "nodev";
+            }
+          ];
+        };
       };
     };
-  };
 }
