de052b070921c734314568a54b4fc6ad987ac66c
bananad3v <bananad3v@proton.me>
2026-08-07 09:38:00 +0300

    treewide: format

--- nixos/activate/systemd.nix
+++ nixos/activate/systemd.nix
@@ -3,39 +3,41 @@ let
   cfg = config.security.nix-secrets;
 in
 {
-  config.systemd.services = lib.mkIf (cfg.enable && cfg.activate.enable && cfg.activate.method == "systemd") {
-    nix-secrets-activate = {
-      wantedBy = [ "sysinit.target" ];
-      after = [
-        "local-fs.target"
-        "systemd-sysusers.service"
-        "userborn.service"
-      ];
-      requiredBy = [ "sysinit-reactivation.target" ];
-      before = [ "sysinit-reactivation.target" ];
-      unitConfig.DefaultDependencies = "no";
-      path = cfg.extraPackages;
+  config.systemd.services =
+    lib.mkIf (cfg.enable && cfg.activate.enable && cfg.activate.method == "systemd")
+      {
+        nix-secrets-activate = {
+          wantedBy = [ "sysinit.target" ];
+          after = [
+            "local-fs.target"
+            "systemd-sysusers.service"
+            "userborn.service"
+          ];
+          requiredBy = [ "sysinit-reactivation.target" ];
+          before = [ "sysinit-reactivation.target" ];
+          unitConfig.DefaultDependencies = "no";
+          path = cfg.extraPackages;
 
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = cfg.activate.command false;
-        RemainAfterExit = true;
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = cfg.activate.command false;
+            RemainAfterExit = true;
+          };
+          # unitConfig.RequiresMountsFor
+        };
+
+        nix-secrets-activate-before-users = {
+          wantedBy = [ "systemd-sysusers.service" ];
+          before = [ "systemd-sysusers.service" ];
+          unitConfig.DefaultDependencies = "no";
+          path = cfg.extraPackages;
+
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = cfg.activate.command true;
+            RemainAfterExit = true;
+          };
+          # unitConfig.RequiresMountsFor
+        };
       };
-      # unitConfig.RequiresMountsFor
-    };
-
-    nix-secrets-activate-before-users = {
-      wantedBy = [ "systemd-sysusers.service" ];
-      before = [ "systemd-sysusers.service" ];
-      unitConfig.DefaultDependencies = "no";
-      path = cfg.extraPackages;
-
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = cfg.activate.command true;
-        RemainAfterExit = true;
-      };
-      # unitConfig.RequiresMountsFor
-    };
-  };
 }
