d3e20b6cfe88e1a855218abbac0cbbcaa7dc94a1
bananad3v <bananad3v@proton.me>
2026-08-14 17:27:20 +0300

    shell: use stable rust
    Signed-off-by: bananad3v <bananad3v@proton.me>

--- shell.nix
+++ shell.nix
@@ -1,23 +1,10 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-let
-  inherit (pkgs) lib;
-  fenix = pkgs.callPackage (pkgs.fetchFromGitHub {
-    owner = "nix-community";
-    repo = "fenix";
-    rev = "42d2c5f54f90dd11ebd5dc7732f004aa900cfecd";
-    hash = "sha256-uexExdN1itvHYHFek4bpvRqURvxYLKWUetpekP+udbk=";
-  }) { };
-
-  toolchain = fenix.combine [
-    fenix.default.toolchain
-    fenix.complete.rust-src
-  ];
-in
 pkgs.callPackage (
   {
     mkShell,
+    pkg-config,
     rustc,
     cargo,
     rustPlatform,
@@ -27,31 +14,17 @@ pkgs.callPackage (
   }:
   mkShell {
     strictDeps = true;
-    nativeBuildInputs = with pkgs; [
+    nativeBuildInputs = [
       pkg-config
-      toolchain
-      llvmPackages.lld
+      rustPlatform.bindgenHook
 
+      rustc
+      cargo
+      rustfmt
       clippy
-      taplo
-      lldb
-
-      nixfmt
+      rust-analyzer
     ];
 
-    env =
-      let
-        inherit (pkgs.llvmPackages) llvm;
-      in
-      {
-        RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
-        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-
-        LLVM_COV = "${llvm}/bin/llvm-cov";
-        LLVM_PROFDATA = "${llvm}/bin/llvm-profdata";
-      }
-      // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-        BINDGEN_EXTRA_CLANG_ARGS = "--sysroot=${pkgs.glibc.dev}";
-      };
+    RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
   }
 ) { }
