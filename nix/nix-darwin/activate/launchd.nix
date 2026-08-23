dc51afb48ab86cb3d98d0a53b3e85fe39f96816b
bananad3v <bananad3v@proton.me>
2026-08-23 13:33:01 +0300

    nix/nix-darwin/activate/launchd: fix rolled back activation

--- nix/nix-darwin/activate/launchd.nix
+++ nix/nix-darwin/activate/launchd.nix
@@ -6,7 +6,17 @@ in
   config.launchd.daemons.nix-secrets-activate =
     lib.mkIf (cfg.enable && cfg.activate.enable && cfg.activate.method == "launchd")
       {
-        scripts = cfg.activate.command false;
+        command = cfg.activate.command false;
+        serviceConfig = {
+          RunAtLoad = true;
+          KeepAlive.SuccessfulExit = false;
+        };
+      };
+
+ config.launchd.daemons.nix-secrets-activate-before-users =
+    lib.mkIf (cfg.enable && cfg.activate.enable && cfg.activate.method == "launchd")
+      {
+        command = cfg.activate.command true;
         serviceConfig = {
           RunAtLoad = true;
           KeepAlive.SuccessfulExit = false;
